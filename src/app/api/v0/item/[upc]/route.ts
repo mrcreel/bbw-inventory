890101219dc118603254fbd2e7be2b9181b3d2e1
mrcreel <michaelrcreel@gmail.com>
2025-03-16 10:48:52 -0500

    feat(prissma): get item from BBW if not in database

--- src/app/api/v0/item/[upc]/route.ts
+++ src/app/api/v0/item/[upc]/route.ts
@@ -1,5 +1,10 @@
 import { promises as fs } from 'fs'
 
+import { NextRequest, NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 type RawItem = {
   Limit: number
   Offset: number
@@ -54,33 +59,71 @@ type Item = {
 }
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { upc: string } },
 ) {
-  const upc = await params.upc
-  console.log(upc)
-  const file = await fs.readFile(`${process.cwd()}/data/items.json`, 'utf8')
-  const data = await JSON.parse(file)
-  const items = await data
-    .filter((rawItem: RawItem) => {
-      return rawItem.Results[0].UPCs.includes(upc)
+  if (!process.env.DATABASE_URL) {
+    return NextResponse.json(
+      { error: 'DATABASE_URL is not set' },
+      { status: 500 },
+    )
+  }
+
+  const { upc } = await params
+  console.log('{upc}: ', upc)
+
+  const item = await prisma.item.findFirst({
+    where: {
+      UPCs: {
+        has: upc,
+      },
+    },
+  })
+
+  if (item) {
+    return Response.json({ item: item })
+  } else {
+    const file = await fs.readFile(
+      `${process.cwd()}/data/item.raw.json`,
+      'utf8',
+    )
+    const data = await JSON.parse(file)
+
+    const rawData = data.filter((rawItemData: RawItem) => {
+      return rawItemData.Results[0].UPCs.includes(upc)
     })
-    .map((rawItem: RawItem) => {
-      const itemData = rawItem.Results[0]
-      const item: Item = {
-        id: itemData.Id,
-        name: itemData.Name,
-        UPCs: itemData.UPCs,
-        description: itemData.Description,
-        imageUrl: itemData.ImageUrl,
-        categoryId: itemData.CategoryId,
-        form: itemData.Attributes.Form.Values[0].Value,
-        collection: itemData.Attributes.Collection.Values[0].Value,
-        productType: itemData.Attributes.Product_Type.Values[0].Value,
-        fragranceName: itemData.Attributes.Fragrance_Name.Values[0].Value,
-        productPageUrl: itemData.ProductPageUrl,
-      }
-      return item
+    const itemData = rawData[0].Results[0]
+
+    const item: Item = {
+      id: itemData.Id,
+      name: itemData.Name,
+      UPCs: itemData.UPCs,
+      description: itemData.Description,
+      imageUrl: itemData.ImageUrl,
+      categoryId: itemData.CategoryId,
+      form: itemData.Attributes.Form.Values[0].Value,
+      collection: itemData.Attributes.Collection.Values[0].Value,
+      productType: itemData.Attributes.Product_Type.Values[0].Value,
+      fragranceName: itemData.Attributes.Fragrance_Name.Values[0].Value,
+      productPageUrl: itemData.ProductPageUrl,
+    }
+
+    const newItem = await prisma.item.create({
+      data: {
+        id: item.id,
+        name: item.name,
+        UPCs: item.UPCs,
+        description: item.description,
+        imageUrl: item.imageUrl,
+        categoryId: item.categoryId,
+        form: item.form,
+        collection: item.collection,
+        productType: item.productType,
+        fragranceName: item.fragranceName,
+        productPageUrl: item.productPageUrl,
+      },
     })
-  return Response.json(items)
+    console.log('newItem:', newItem)
+    return Response.json({ newItem: newItem })
+  }
 }
